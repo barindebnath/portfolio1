1ede698dcabcfd9cef91aaec3fb736c188723a05
Barin <barindebnath@gmail.com>
2020-08-08 12:22:23 +0530

    Updates

---
+++ precache-manifest.bde2e10b520dee3ad870fcd97b08898e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913cd59a83a57684c04419c96950a3cc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "367dddb9019df8130ec0",
+    "url": "/portfolio/static/css/main.669d3a93.chunk.css"
+  },
+  {
+    "revision": "b7e4b4889d4ac49e5016",
+    "url": "/portfolio/static/js/2.6da26198.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/portfolio/static/js/2.6da26198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367dddb9019df8130ec0",
+    "url": "/portfolio/static/js/main.f08f4ea9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1124f2dfb9aded88ff17defb38693168",
+    "url": "/portfolio/static/media/DP.1124f2df.jpg"
+  },
+  {
+    "revision": "55444e352f7526f110e6f2a5fd3fdd7a",
+    "url": "/portfolio/static/media/Resume.55444e35.pdf"
+  },
+  {
+    "revision": "1d4785e885cc0c04ea283612dff868df",
+    "url": "/portfolio/static/media/msbte.1d4785e8.jpg"
+  },
+  {
+    "revision": "d023d5f8d6229abe27a440057eef9174",
+    "url": "/portfolio/static/media/prostoker.d023d5f8.PNG"
+  },
+  {
+    "revision": "6fc4498c9bcf3eaa74d1781529bf9f02",
+    "url": "/portfolio/static/media/py.6fc4498c.png"
+  },
+  {
+    "revision": "962aab72f96d5cc7282027121b817db0",
+    "url": "/portfolio/static/media/scoe.962aab72.PNG"
+  },
+  {
+    "revision": "dfbfec24591b23bfe1f92167523448b8",
+    "url": "/portfolio/static/media/sppu.dfbfec24.jpg"
+  },
+  {
+    "revision": "8a3110a8ae3fca9c370b5d1d19e87c14",
+    "url": "/portfolio/static/media/ssc.8a3110a8.png"
+  },
+  {
+    "revision": "0c5072d6728a1ec9361cfc3516e173e2",
+    "url": "/portfolio/static/media/wtsppu.0c5072d6.PNG"
+  },
+  {
+    "revision": "e1ee951a9ba0499fb45e19cfc01beb4d",
+    "url": "/portfolio/static/media/wtsppu2.e1ee951a.PNG"
+  }
+]);

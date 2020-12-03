14499e392fcca222a494e67f7adcf072ae33cd79
unknown <barindebnath@gmail.com>
2020-12-03 15:57:15 +0530

    marks updated

--- src/Skill.js
+++ src/Skill.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { Title } from "./CardTitle";
 import { Line } from "./CardLine";
-import Py from "./img/py.png";
+//import Py from "./img/py.png";
 
 export const Skill = () => {
   return (
     <>
       <div className="cW350 card mx-2 d-inline-block rounded-lg">
         <div className="card-body rounded-lg">
-          <Title title="Frameworks" />
+          <Title title="Programming Languages" />
           <hr />
+          <Line small="React Framework" big="React Native" />
           <Line small="JavaScript  Library" big="React" />
-          <Line small="Software" big="Django" />
-          <Line small="CSS Framework" big="Bootstrap" />
+          <Line small="Language" big="JavaScript" />
         </div>
         <i className="fab fa-react card-img-top fa-5x" style={{ color: "#61DBFB" }}></i>
       </div>
-      <div className="cW350 card mx-2 d-inline-block rounded-lg">
+      {/* <div className="cW350 card mx-2 d-inline-block rounded-lg">
         <div className="card-body rounded-lg">
           <Title title="Front-End Technologies" />
           <hr />
-          {/* <Line small="Programming Language" big="TypeScript" /> */}
+          <Line small="Programming Language" big="TypeScript" />
           <Line small="Style Sheet Language" big="CSS3" />
           <Line small="Programming Language" big="SCSS" />
           <Line small="JavaScript  Library" big="jQuery" />
@@ -44,7 +44,7 @@ export const Skill = () => {
         </div>
         <i className="fas fa-chart-pie card-img-top fa-5x text-primary"></i>
       </div>
-      {/* <div className="cW350 card mx-2 d-inline-block rounded-lg">
+      <div className="cW350 card mx-2 d-inline-block rounded-lg">
         <div className="card-body rounded-lg">
           <Title title="Upcoming in List" />
           <Line big="Redux" />

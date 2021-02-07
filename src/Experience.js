bdf2850ed6566bf4981a3d97d587f583252c9ec0
unknown <barindebnath@gmail.com>
2021-02-07 13:27:38 +0530

    add exp

--- src/Experience.js
+++ src/Experience.js
@@ -5,14 +5,24 @@ import { Line } from "./CardLine";
 export const Experience = () => {
   return (
     <>
-      <div className="card mx-2 d-inline-block rounded-lg">
-        <div className="card-body rounded-lg">
-          <Title title="Frontend Developer" />
+      <div className='card mx-2 d-inline-block rounded-lg'>
+        <div className='card-body rounded-lg'>
+          <Title title='Web Developer' />
           <hr />
-          <Line small="Company" big="Puspendu Studio" />
-          <Line small="From - To" big="Sep 2019 - Sep 2020" />
-          <Line small="Location" big="Nashik, Maharashtra, India" />
-          <Line small="Technology" big="CSS3, Bootstrap4, JavaScript, jQuery" />
+          <Line small='Company' big='AxelBuzz Tech Solutions' />
+          <Line small='From - To' big='Dec 2020 - Present' />
+          <Line small='Location' big='Nashik, Maharashtra, India' />
+          <Line small='Technology' big='JavaScript, React.js' />
+        </div>
+      </div>
+      <div className='card mx-2 d-inline-block rounded-lg'>
+        <div className='card-body rounded-lg'>
+          <Title title='Frontend Developer' />
+          <hr />
+          <Line small='Company' big='Puspendu Studio' />
+          <Line small='From - To' big='Sep 2019 - Sep 2020' />
+          <Line small='Location' big='Nashik, Maharashtra, India' />
+          <Line small='Technology' big='CSS3, Bootstrap4, JavaScript, jQuery' />
         </div>
       </div>
     </>

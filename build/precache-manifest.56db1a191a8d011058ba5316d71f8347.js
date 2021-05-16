521bf7e8f9c4dbbdb8becc0d1ae6da6cc78f1fb6
unknown <barindebnath@gmail.com>
2021-05-16 12:48:32 +0530

    unknown

---
+++ build/precache-manifest.56db1a191a8d011058ba5316d71f8347.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edcb4dcfc5b5614ae350c8379fa0eeb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f9c5db156e69065c8691",
+    "url": "/portfolio/static/css/main.eda738fd.chunk.css"
+  },
+  {
+    "revision": "eb2be312336214889e12",
+    "url": "/portfolio/static/js/2.52c26405.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.52c26405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c5db156e69065c8691",
+    "url": "/portfolio/static/js/main.2645dbdb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1124f2dfb9aded88ff17defb38693168",
+    "url": "/portfolio/static/media/DP.1124f2df.jpg"
+  },
+  {
+    "revision": "59bd60ce04d7564d751aaffdc7429185",
+    "url": "/portfolio/static/media/Resume.59bd60ce.pdf"
+  },
+  {
+    "revision": "1d4785e885cc0c04ea283612dff868df",
+    "url": "/portfolio/static/media/msbte.1d4785e8.jpg"
+  },
+  {
+    "revision": "d023d5f8d6229abe27a440057eef9174",
+    "url": "/portfolio/static/media/prostoker.d023d5f8.PNG"
+  },
+  {
+    "revision": "962aab72f96d5cc7282027121b817db0",
+    "url": "/portfolio/static/media/scoe.962aab72.PNG"
+  },
+  {
+    "revision": "dfbfec24591b23bfe1f92167523448b8",
+    "url": "/portfolio/static/media/sppu.dfbfec24.jpg"
+  },
+  {
+    "revision": "8a3110a8ae3fca9c370b5d1d19e87c14",
+    "url": "/portfolio/static/media/ssc.8a3110a8.png"
+  },
+  {
+    "revision": "0c5072d6728a1ec9361cfc3516e173e2",
+    "url": "/portfolio/static/media/wtsppu.0c5072d6.PNG"
+  },
+  {
+    "revision": "dddabc87522694614a70aaff831d14cd",
+    "url": "/portfolio/static/media/wtsppu2.dddabc87.jpg"
+  }
+]);

c9fbf7dbb22c499c9df52941dc6424e0ceee1a90
unknown <barindebnath@gmail.com>
2021-10-11 00:22:53 +0530

    add adroitcoders

--- src/Sections/Experience.js
+++ src/Sections/Experience.js
@@ -4,19 +4,28 @@ import { Title, Line } from "./CardText";
 const Experience = () => {
   const experiences = [
     {
-      title: "Web Developer",
+      title: "React.js Developer",
+      lines: [
+        { small: "Company", big: "Adroit Coders" },
+        { small: "From - To", big: "Sep 2021 - Present" },
+        { small: "Location", big: "Nashik, Maharashtra, India" },
+        { small: "Technology", big: "React.js, TypeScript" },
+      ],
+    },
+    {
+      title: "React.js Developer",
       lines: [
         { small: "Company", big: "AxelBuzz Tech Solutions" },
-        { small: "From - To", big: "Dec 2020 - Present" },
+        { small: "From - To", big: "Dec 2020 - Sep 2021 (9 Months)" },
         { small: "Location", big: "Nashik, Maharashtra, India" },
-        { small: "Technology", big: "JavaScript, React.js" },
+        { small: "Technology", big: "React.js, JavaScript" },
       ],
     },
     {
       title: "Frontend Developer",
       lines: [
         { small: "Company", big: "Puspendu Studio" },
-        { small: "From - To", big: "Sep 2019 - Sep 2020" },
+        { small: "From - To", big: "Sep 2019 - Aug 2020 (1 Year)" },
         { small: "Location", big: "Nashik, Maharashtra, India" },
         { small: "Technology", big: "CSS3, Bootstrap4, JavaScript, jQuery" },
       ],

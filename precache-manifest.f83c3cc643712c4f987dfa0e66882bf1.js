f36c8bab13fd9002fb3536fa3900761da57e2d8b
Barin <barindebnath@gmail.com>
2021-10-24 20:01:15 +0530

    Updates

---
+++ precache-manifest.f83c3cc643712c4f987dfa0e66882bf1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f6f8d652a7265594bb5c126286e0c6",
+    "url": "/portfolio1/index.html"
+  },
+  {
+    "revision": "2db9227b2215fcddd8fd",
+    "url": "/portfolio1/static/css/main.eda738fd.chunk.css"
+  },
+  {
+    "revision": "af6e7f31e483caec4ab3",
+    "url": "/portfolio1/static/js/2.75149408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio1/static/js/2.75149408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db9227b2215fcddd8fd",
+    "url": "/portfolio1/static/js/main.7c1ebb1d.chunk.js"
+  },
+  {
+    "revision": "43cb86accc6e0f3720db",
+    "url": "/portfolio1/static/js/runtime-main.1ca99ad0.js"
+  },
+  {
+    "revision": "1124f2dfb9aded88ff17defb38693168",
+    "url": "/portfolio1/static/media/DP.1124f2df.jpg"
+  },
+  {
+    "revision": "59bd60ce04d7564d751aaffdc7429185",
+    "url": "/portfolio1/static/media/Resume.59bd60ce.pdf"
+  },
+  {
+    "revision": "1d4785e885cc0c04ea283612dff868df",
+    "url": "/portfolio1/static/media/msbte.1d4785e8.jpg"
+  },
+  {
+    "revision": "d023d5f8d6229abe27a440057eef9174",
+    "url": "/portfolio1/static/media/prostoker.d023d5f8.PNG"
+  },
+  {
+    "revision": "962aab72f96d5cc7282027121b817db0",
+    "url": "/portfolio1/static/media/scoe.962aab72.PNG"
+  },
+  {
+    "revision": "dfbfec24591b23bfe1f92167523448b8",
+    "url": "/portfolio1/static/media/sppu.dfbfec24.jpg"
+  },
+  {
+    "revision": "8a3110a8ae3fca9c370b5d1d19e87c14",
+    "url": "/portfolio1/static/media/ssc.8a3110a8.png"
+  },
+  {
+    "revision": "0c5072d6728a1ec9361cfc3516e173e2",
+    "url": "/portfolio1/static/media/wtsppu.0c5072d6.PNG"
+  },
+  {
+    "revision": "dddabc87522694614a70aaff831d14cd",
+    "url": "/portfolio1/static/media/wtsppu2.dddabc87.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.43c2de7bc4d62315510d603f0501ea0f.js"
+  "/portfolio1/precache-manifest.f83c3cc643712c4f987dfa0e66882bf1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
